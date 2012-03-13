c322f725ae4490270f207b3fb3456cd4371a3efe
Morten Kjaergaard <mkjargaard@willowgarage.com>
2012-03-12 17:44:22 -0700

    link to rosconsole

--- tf2/catkin.cmake
+++ tf2/catkin.cmake
@@ -2,12 +2,14 @@ cmake_minimum_required(VERSION 2.8)
 project(tf2)
 
 find_package(catkin)
-find_package(ROS REQUIRED COMPONENTS roscpp)
+find_package(ROS REQUIRED COMPONENTS rosconsole)
 
 include_directories(include)
 include_directories(${ROS_INCLUDE_DIRS})
 
 add_library(tf2 SHARED src/cache.cpp src/buffer_core.cpp src/static_cache.cpp)
+target_link_libraries(tf2 ${ROS_LIBRARIES})
 
 catkin_project(tf2
-  INCLUDE_DIRS include)
+  INCLUDE_DIRS include
+  DEPENDS rosconsole)
